d9af451910492151c9a3df31af42d36414d3f826
sarana <saranasudsang@gmail.com>
2022-05-04 20:23:16 +0700

    040565

--- app_food/urls.py
+++ app_food/urls.py
@@ -1,4 +1,3 @@
-from http.client import HTTPResponse
 from django.urls import path
 from . import views
 
--- app_food/views.py
+++ app_food/views.py
@@ -1,10 +1,30 @@
+from datetime import datetime
 from django.shortcuts import render
-from django.http.response import HttpResponse
 
 # Create your views here.
 
+all_foods = [
+    {'id' : 1, 'title' : 'Dark choco premium'
+    , 'price' : 2499, 'is_premium' : True
+    , 'promotion_end_at' : datetime(2022, 2, 28)},
+    {'id' : 2, 'title' : 'Red Spicy'
+    , 'price' : 349, 'is_premium' : False
+    , 'promotion_end_at' : datetime(2022, 2, 15)},
+    {'id' : 3, 'title' : 'Blue Glacier'
+    , 'price' : 349, 'is_premium' : False
+    , 'promotion_end_at' : datetime(2022, 2, 15)},
+]
+
 def foods(request):
-    return render(request, 'app_food/foods.html')
+    context = {'foods' : all_foods}
+    return render(request, 'app_food/foods.html', context)
 
 def food(request, food_id):
-    return render(request, 'app_food/food.html', context={'food_id': food_id})
+    one_food = None
+    try:
+        one_food = [f for f in all_foods if f['id'] == food_id][0]
+    except IndexError:
+        print('Index Error')
+    context = {'food' : one_food}
+    return render(request,
+     'app_food/food.html', context)
